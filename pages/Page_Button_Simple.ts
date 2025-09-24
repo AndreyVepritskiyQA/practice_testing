5707f97ec1e378c16e8ba85c29c04e0b55dff52e
Andrei Vepritskiy <andrei.vepritskiy@gmail.com>
2025-09-24 11:08:02 +0300

    Добавлена страница тестирования Button

---
+++ pages/Page_Button_Simple.ts
@@ -0,0 +1,32 @@
+import { expect, Locator, Page } from '@playwright/test';
+
+export class PageButtonSimple {
+  baseURL: string = 'https://www.qa-practice.com/elements/button/simple';
+  private page: Page;
+  readonly buttonLocator: Locator;
+  readonly responseLocator: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.buttonLocator = page.locator('#submit-id-submit');
+    this.responseLocator = page.locator('#result-text');
+  }
+
+  //Методы для тестов
+
+  async OpenURL() {
+    await this.page.goto(this.baseURL);
+  }
+
+  async ValidateButtonLabel(label: string) {
+    await expect(this.buttonLocator).toHaveText(label);
+  }
+
+  async ClickButton() {
+    await this.buttonLocator.click();
+  }
+
+  async ValidateResponse(responseText: string) {
+    await expect(this.responseLocator).toHaveText(responseText);
+  }
+}
